343dd83a6bff63832e994d10a656790643a007ce
LeeYunHwan <cyzhqly@gmail.com>
2024-10-01 15:28:18 +0900

    feat: todo 개별 수정 및 낙관적 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,66 @@
-import React, { useState, useEffect } from "react";
-import LoadingBar from "./components/LoadingBar";
-import TodoItem from "./components/TodoItem";
+import React, { useState, useEffect } from 'react'
+import LoadingBar from './components/LoadingBar'
+import TodoItem from './components/TodoItem'
 
-export type Todos = Todo[];
+export type Todos = Todo[]
 export interface Todo {
-  id: string;
-  order: number;
-  title: string;
-  done: boolean;
-  createdAt: string;
-  updatedAt: string;
+  id: string
+  order: number
+  title: string
+  done: boolean
+  createdAt: string
+  updatedAt: string
 }
 export default function App() {
-  const [todos, setTodos] = useState<Todos>([]); 
-  const [message, setMessage] = useState(""); // 타입 추론(inference) : ""
-  const [loading, setLoading] = useState(true);
-
+  const [todos, setTodos] = useState<Todos>([])
+  const [message, setMessage] = useState('') // 타입 추론(inference) : ""
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    getTodos();
-  }, []);
+    getTodos()
+  }, [])
 
   async function getTodos() {
-    try {      //! 이행
-      const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos",{
-        // method: 'GET', // 기본값, 생략 가능!!!
-        headers: {
-          'content-type': 'application/json',
-          apikey: '5X8Z1k7M2vU5Q',
-          username: 'Grepp_KDT4_ParkYoungWoong',
+    try {
+      //! 이행
+      const res = await fetch(
+        'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+        {
+          // method: 'GET', // 기본값, 생략 가능!!!
+          headers: {
+            'content-type': 'application/json',
+            apikey: '5X8Z1k7M2vU5Q',
+            username: 'Grepp_KDT4_ParkYoungWoong'
+          }
         }
-      });
+      )
       // const res = await fetch("../public/todo_list.json");
 
-      const data = await res.json();
-      console.log("응답 결과: ", data);
-      setTodos(data);
-    } catch (error) {      //! 거부
+      const data = await res.json()
+      console.log('응답 결과: ', data)
+      setTodos(data)
+    } catch (error) {
+      //! 거부
       if (error instanceof Error) {
-        const message = "에러가 발생했어요!";
-        console.error("에러 발생: ", message);
-        setMessage(message);
+        const message = '에러가 발생했어요!'
+        console.error('에러 발생: ', message)
+        setMessage(message)
       } // 타입 가드
-    } finally {      //! 무조건 실행
-      setLoading(false);
+    } finally {
+      //! 무조건 실행
+      setLoading(false)
     }
+  }
+
+  function setTodo(updatedTodo: Todo) {
+    setTodos(todos => {
+      return todos.map(todo => {
+        if(todo.id === updatedTodo.id){
+          return updatedTodo;
+        } 
+        return todo;
+      })
+    })
   }
 
   return (
@@ -52,12 +68,12 @@ export default function App() {
       <div>{loading && <LoadingBar />}</div>
       <div>{message}</div>
       <ul>
-        {todos.map((todo) => (
+        {todos.map(todo => (
           <React.Fragment key={todo.id}>
-            <TodoItem todoItem={todo} getTodos={getTodos}/>
+            <TodoItem todoItem={todo} setTodo={setTodo}/>
           </React.Fragment>
         ))}
       </ul>
     </>
-  );
+  )
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,52 +1,69 @@
-import { useState } from "react";
-import { Todo } from "../App";
+import { useState } from 'react'
+import { Todo } from '../App'
 
-export default function TodoItem({ 
-  todoItem, 
-  getTodos 
-}: { 
-  todoItem: Todo; 
-  getTodos:() => void; // getTodos가 매개변수가 없고, return값도 없음. 
-}) {
-  const [title, setTitle] = useState(todoItem.title); // name으로 각 input창 초기화
-  
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
-    e.preventDefault();
-    updateTodo(todoItem, title);
+export default function TodoItem({
+   todoItem,
+    setTodo 
+  }: { 
+    todoItem: Todo;
+    setTodo: (updatedTodo: Todo) => void;
+  }) {
+  const [title, setTitle] = useState(todoItem.title) // name으로 각 input창 초기화
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    updateTodo(todoItem, title)
   }
 
-  async function updateTodo(todoItem: Todo, title: string) { //코드 추상화 : 함수 이름만으로 무슨 로직인지 쉽게 알 수 있도록
-    console.log('서버로 전송!', title);
+  async function updateTodo(todoItem: Todo, title: string) {
+    //코드 추상화 : 함수 이름만으로 무슨 로직인지 쉽게 알 수 있도록
     
-    const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${todoItem.id}`,{ // 이 데이터의 id부분을 수정하려해.
-      method: 'PUT',
-      headers: {
-        'content-type': 'application/json',
-        apikey: '5X8Z1k7M2vU5Q',
-        username: 'Grepp_KDT4_ParkYoungWoong',
-      },
-      body: JSON.stringify({
-        title: title, // : title 생략가능! -> 속성과 값의 이름이 같아서
-        done: todoItem.done,
-      })
-    });
-    // 위에서 응답받은 데이터를 사용하기 위한 코드
-    const data = await res.json(); // 데이터 분석
-    console.log(data);
+    //낙관적 업데이트
+    setTodo({...todoItem, title})
+    try{
+      const res = await fetch(
+        `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${todoItem.id}`,
+        {
+          // 이 데이터에서 해당id를 가진 데이터를 수정하려해.
+          method: 'PUT',
+          headers: {
+            'content-type': 'application/json',
+            apikey: '5X8Z1k7M2vU5Q',
+            username: 'Grepp_KDT4_ParkYoungWoong'
+          },
+          body: JSON.stringify({
+            title: title, // : 값 title 생략가능! -> 속성과 값의 이름이 같아서
+            done: todoItem.done
+          })
+        }
+      )
 
-    getTodos();
+      const updatedTodo: Todo = await res.json() // 데이터 분석
+      console.log(updatedTodo, title)
+    } catch (error) {
+      console.error(error);
+      setTodo(todoItem);
+    }
+    
+    // 위에서 응답받은 데이터를 사용하기 위한 코드
+    
+    
   }
 
   async function deleteTodo(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${todoItem.id}`,{ // 이 id를 가진 데이터를 삭제하려해.
-      method: 'DELETE',
-      headers: {
-        'content-type': 'application/json',
-        apikey: '5X8Z1k7M2vU5Q',
-        username: 'Grepp_KDT4_ParkYoungWoong',
+    e.preventDefault()
+    await fetch(
+      `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${todoItem.id}`,
+      {
+        // 이 id를 가진 데이터를 삭제하려해.
+        method: 'DELETE',
+        headers: {
+          'content-type': 'application/json',
+          apikey: '5X8Z1k7M2vU5Q',
+          username: 'Grepp_KDT4_ParkYoungWoong'
+        }
       }
-    }); //전송만 함. 응답은 받아도 쓸데가 없음.
+    ) //전송만 함. 응답은 받아도 쓸데가 없음.
 
     //삭제는 사용/분석할 데이터가 없음, fetch 이후 true/false값만 반환
   }
@@ -55,11 +72,13 @@ export default function TodoItem({
     <li>
       <form onSubmit={handleSubmit}>
         {todoItem.title}
-        <input value={title} onChange={(e)=> setTitle(e.target.value)}/>
-        <button type='submit'>수정</button>
+        <input
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <button type="submit">수정</button>
         <button onClick={deleteTodo}>삭제</button>
       </form>
     </li>
-  );
+  )
 }
-
