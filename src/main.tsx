f9850001bddec50a78a594312c921aaced81e9d3
LeeYunHwan <cyzhqly@gmail.com>
2024-10-19 00:10:57 +0900

    chore: index.css 연동 제거 #9

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes'; //index.tsx는 생략 가능
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
